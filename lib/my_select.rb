53677861112ae68c922aebd3b90b86a4bdbe52d1
Shawn Ren <doggy-ren@hotmail.com>
2019-03-05 16:56:22 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,18 @@
 def my_select(collection)
   i = 0
   newArray=[]
+  def (num).even?
+    if num % 2 == 0
+      return true
+    else
+      return false
+    end
+  end
   if collection.length == 0
     puts "This block should not run!"
   else
     while i < collection.length
-      if collection[i] % 2 == 0
+      if collection[i].even?
         yield newArray.push(collection[i])
       end
       i += 1
